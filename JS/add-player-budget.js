fa1924fc503d588237765eb20d5e4c82d7bb57d2
MazharulUtsho <mazharul33-678@diu.edu.bd>
2022-08-25 22:29:56 +0600

    final-selected-condition-added

--- JS/add-player-budget.js
+++ JS/add-player-budget.js
@@ -17,3 +17,11 @@ document.getElementById('ramos-button').addEventListener('click', function () {
 document.getElementById('sanches-button').addEventListener('click', function () {
     playerNameAddedToSelectedV('sanches-title', 'sanches-inside-list', 'sanches-button');
 })
+document.getElementById('machado-button').addEventListener('click', function () {
+    const playerNameList = document.querySelectorAll(".player-name");
+    const playerNameLength = playerNameList.length;
+    if (playerNameLength >= 5) {
+        alert('can not add more than five player');
+        return;
+    }
+})
